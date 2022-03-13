b7b76960afe3145c5be00b3e12dafc6b79b4180d
bjh7013 <60734701+bjh7013@users.noreply.github.com>
2022-03-14 00:55:55 +0900

    Create 35. Search Insert Position.java
    35. Search Insert Position Solution java

---
+++ LeetCode/35. Search Insert Position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int index = 0;
+        
+        for(int i =  0 ; i < nums.length; i++) {
+            if(nums[i] == target) {
+                index = i;
+                break;
+            }else if (nums[i] < target) {
+                index++;
+            }else{
+                break;
+            }
+        }
+
+        return index;
+    }
+}
